0e6242235153640c8adf104cffb9902240eb5b12
Ylona TARDIF <yltardif@e1r5p13.42.fr>
2020-01-06 15:18:04 +0100

    yo frero c'est bientot fini

--- ft_itoa.c
+++ ft_itoa.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_itoa.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yltardif <yltardif@student.42>             +#+  +:+       +#+        */
+/*   By: yltardif <yltardif@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/05 18:34:19 by yltardif          #+#    #+#             */
-/*   Updated: 2020/01/06 14:21:11 by yltardif         ###   ########.fr       */
+/*   Updated: 2020/01/06 15:16:33 by yltardif         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,9 @@ char	*ft_itoa(int n)
 	int len;
 	int sign;
 
-	len = 1;
-	sign = 0;
-	if (n < 0)
-	{
-		sign = 1;
-		n = n * -1;
-		len++;
-	}
-	nb = n;
+	sign = n < 0 ? 1 : 0;
+	nb = n < 0 ? n * -1 : n;
+	len = n < 0 ? 2 : 1;
 	while (n /= 10)
 		len++;
 	if (!(str = (char *)malloc(sizeof(char) * (len + 1))))
@@ -44,11 +38,3 @@ char	*ft_itoa(int n)
 		str[--len] = '-';
 	return (str);
 }
-
-// int	main()
-// {
-// 	char *str;
-// 	str = ft_itoa(0);
-// 	printf("%s\n", str);
-// 	return (0);
-// }
